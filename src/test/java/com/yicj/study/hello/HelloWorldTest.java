9675e8917b75f79ab9bb86184a35c384355efb34
易成杰 <626659321@qq.com>
2020-08-18 11:10:50 +0800

    单元测试无法运行解决

---
+++ src/test/java/com/yicj/study/hello/HelloWorldTest.java
@@ -0,0 +1,22 @@
+package com.yicj.study.hello;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * ClassName: HelloWorldTest
+ * Description: TODO(描述)
+ * Date: 2020/8/18 10:03
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void test1(){
+
+        System.out.println("test");
+    }
+
+}
